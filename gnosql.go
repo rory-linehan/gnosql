36c4e19f6c7165ddccb1497aac97d6d532f3e6a4
Rory Linehan <rory@jyro.io>
2020-06-24 13:52:35 -0500

    setting up init(), adding logging, reformatting code

--- gnosql.go
+++ gnosql.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	log "github.com/sirupsen/logrus"
 	"io/ioutil"
-	"log"
 	"net"
 	"net/http"
+	"os"
 
 	"github.com/davecgh/go-spew/spew"
 	"gopkg.in/yaml.v2"
@@ -18,6 +19,7 @@ type conf struct {
 	Persistence bool   `yaml:"Persistence"`
 	CertFile    string `yaml:"CertFile"`
 	KeyFile     string `yaml:"KeyFile"`
+	LogLevel    string `yaml:"LogLevel"`
 }
 
 func executeDatabaseOperation() {
@@ -29,20 +31,20 @@ func handleRequest(conn net.Conn) {
 }
 
 func server(config conf) {
-	if config.Protocol == "HTTPS" {
-		http.ListenAndServeTLS(
-			config.Address+":"+string(config.Port),
-			config.CertFile,
-			config.KeyFile,
-			nil)
-	} else if config.Protocol == "HTTP" {
+	contextLogger := log.WithFields(log.Fields{"function": "server"})
+	switch config.Protocol {
+	case "HTTPS":
+		contextLogger.Info("starting HTTPS server")
+		http.ListenAndServeTLS(config.Address+":"+string(config.Port), config.CertFile, config.KeyFile, nil)
+	default:
+		contextLogger.Info("starting default HTTP server")
 		http.ListenAndServe(config.Address+":"+string(config.Port), nil)
-	} else {
-
 	}
 }
 
 func getConfig() conf {
+	contextLogger := log.WithFields(log.Fields{"function": "getConfig"})
+	contextLogger.Info("loading config.yaml")
 	var c conf
 	yamlFile, err := ioutil.ReadFile("etc/config.yaml")
 	if err != nil {
@@ -55,8 +57,36 @@ func getConfig() conf {
 	return c
 }
 
+func init() {
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.ErrorLevel)
+}
+
 func main() {
+	contextLogger := log.WithFields(log.Fields{"function": "main"})
 	config := getConfig()
 	spew.Dump(config)
-	go server(config)
+
+	contextLogger.Info("setting log output level from config.LogLevel")
+	switch config.LogLevel {
+	case "TRACE":
+		log.SetLevel(log.TraceLevel)
+	case "DEBUG":
+		log.SetLevel(log.DebugLevel)
+	case "INFO":
+		log.SetLevel(log.InfoLevel)
+	case "WARN":
+		log.SetLevel(log.WarnLevel)
+	case "ERROR":
+		log.SetLevel(log.ErrorLevel)
+	case "FATAL":
+		log.SetLevel(log.FatalLevel)
+	case "PANIC":
+		log.SetLevel(log.PanicLevel)
+	default:
+		log.SetLevel(log.ErrorLevel)
+	}
+
+	server(config)
 }
