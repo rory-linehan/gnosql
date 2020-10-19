241d6d28be09d6b894441f715c0ebbc68a0a4e2a
rorylinehan@protonmail.com <rory@jyro.io>
2020-10-18 19:02:40 -0500

    updating .gitignore, adding databaseResponse struct to database.go

--- internal/database/database.go
+++ internal/database/database.go
@@ -10,32 +10,50 @@ func syncToDisk() {
 }
 
 type databaseResponse struct {
+	Code int
+	Body []byte
 }
 
 func Select(res http.ResponseWriter, req *http.Request) {
 	contextLogger := log.WithFields(log.Fields{"function": "database.Select"})
 	contextLogger.Debug("handling /select request")
-	res.WriteHeader(200)
-	res.Write([]byte("select"))
+	resp := databaseResponse{
+		Code: 200,
+		Body: []byte("select"),
+	}
+	res.WriteHeader(resp.Code)
+	res.Write(resp.Body)
 }
 
 func Insert(res http.ResponseWriter, req *http.Request) {
 	contextLogger := log.WithFields(log.Fields{"function": "database.Insert"})
 	contextLogger.Debug("handling /insert request")
-	res.WriteHeader(200)
-	res.Write([]byte("insert"))
+	resp := databaseResponse{
+		Code: 200,
+		Body: []byte("insert"),
+	}
+	res.WriteHeader(resp.Code)
+	res.Write(resp.Body)
 }
 
 func Update(res http.ResponseWriter, req *http.Request) {
 	contextLogger := log.WithFields(log.Fields{"function": "database.Update"})
 	contextLogger.Debug("handling /update request")
-	res.WriteHeader(200)
-	res.Write([]byte("update"))
+	resp := databaseResponse{
+		Code: 200,
+		Body: []byte("update"),
+	}
+	res.WriteHeader(resp.Code)
+	res.Write(resp.Body)
 }
 
 func Delete(res http.ResponseWriter, req *http.Request) {
 	contextLogger := log.WithFields(log.Fields{"function": "database.Delete"})
 	contextLogger.Debug("handling /delete request: ")
-	res.WriteHeader(200)
-	res.Write([]byte("delete"))
+	resp := databaseResponse{
+		Code: 200,
+		Body: []byte("delete"),
+	}
+	res.WriteHeader(resp.Code)
+	res.Write(resp.Body)
 }
